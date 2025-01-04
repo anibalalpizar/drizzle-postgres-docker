257ac4e4fd0d849242a90ed11133be00793425eb
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-04 01:50:41 -0600

    fix: ensure DATABASE_URL is defined in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 
+const DB_URL = process.env.DATABASE_URL
+
+if (!DB_URL) throw new Error("DATABASE_URL is required")
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: DB_URL,
   },
 })
