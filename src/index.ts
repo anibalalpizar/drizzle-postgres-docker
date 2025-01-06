4c70dadc5a27b512ef8b020a729d12b1d4b4d42d
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-05 19:34:51 -0600

    feat: enhance user management with update and delete functionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import "dotenv/config"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { usersTable, type User } from "./db/schema"
+import { and, eq } from "drizzle-orm"
+import * as schema from "./db/schema"
 
 const DB_URL = process.env.DATABASE_URL
 
 if (!DB_URL) throw new Error("DATABASE_URL is required")
 
-const db = drizzle(DB_URL)
+const db = drizzle(DB_URL, { schema })
 
 async function main() {
   await db.delete(usersTable)
@@ -21,7 +23,36 @@ async function main() {
 
   const users = await db.select().from(usersTable)
 
+  console.log("User inserted")
   console.log(users)
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(
+      and(
+        eq(usersTable.email, newuser.email),
+        eq(usersTable.name, newuser.name)
+      )
+    )
+
+  const usersAfterUpdate = await db.query.usersTable.findMany()
+
+  console.log("User updated")
+  console.log(usersAfterUpdate)
+
+  const findedUser = await db.query.usersTable.findFirst({
+    where: (users, { eq }) => eq(users.email, newuser.email),
+  })
+
+  console.log("User finded")
+  console.log(findedUser)
+
+  await db.delete(usersTable).where(eq(usersTable.email, "john@vercel.com"))
+
+  console.log("User deleted")
 }
 
 main().catch(console.error)
