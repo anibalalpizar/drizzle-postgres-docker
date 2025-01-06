7c94574793daf28dfe2b4356a232ff589da859e6
anibalalpizar <anibal.alpizar14@gmail.com>
2025-01-05 19:52:26 -0600

    feat: add posts table and define relations with users

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
 
 export const usersTable = pgTable("users", {
@@ -7,4 +8,22 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
 })
 
+export const postsTable = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  content: varchar({ length: 255 }).notNull(),
+  userId: integer().notNull(),
+})
+
 export type User = typeof usersTable.$inferInsert
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  post: many(postsTable),
+}))
+
+export type Post = typeof postsTable.$inferInsert
+export const postsRelations = relations(postsTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [postsTable.userId],
+    references: [usersTable.id],
+  }),
+}))
